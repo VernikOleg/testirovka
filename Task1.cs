1e649a2d6b8dcf31f3cc555230b748907d830fec
VernikOleg <72952451+VernikOleg@users.noreply.github.com>
2021-05-18 02:27:13 +0300

    Added Task 1-3
    Nothing important changed

--- Task1.cs
+++ Task1.cs
@@ -1,4 +1,4 @@
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using System;
@@ -7,41 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 
-namespace Lab3TestCases
+namespace Lab3Tests
 {
-    class Task1
+    public class Tests
     {
         IWebDriver driver;
-
         [SetUp]
-        public void StartBrowser()
+        public void Setup()
         {
-            driver = new FirefoxDriver();
-            driver.Manage().Window.Maximize();
+            driver = new OpenQA.Selenium.Firefox.FirefoxDriver();
         }
 
         [Test]
-        public void Test()
+        public void Test1()
         {
-            // Checking schedule of student group
-
-            String targer = "КП-92";
-            driver.Navigate().GoToUrl("http://rozklad.kpi.ua/Schedules/ScheduleGroupSelection.aspx");
-
-            //Searching for target
-            driver.FindElement(By.Id("ctl00_MainContent_ctl00_txtboxGroup")).SendKeys(targer);
+            //search
+            driver.Navigate().GoToUrl("https://www.google.com/");       
+            driver.FindElement(By.Name("q")).SendKeys("Розклад КПІ");
+            driver.FindElement(By.Name("q")).Submit();
+            driver.FindElement(By.ClassName("yuRUbf")).Click();
+            //check
+            driver.FindElement(By.Id("ctl00_MainContent_ctl00_txtboxGroup")).SendKeys("КП-92");
+            driver.FindElement(By.Id("ctl00_MainContent_ctl00_txtboxGroup")).Click();
             driver.FindElement(By.Id("ctl00_MainContent_ctl00_btnShowSchedule")).Click();
-
-            //Comparing results
             String actualResult = driver.FindElement(By.XPath("/html/body/div/form/div[5]/div[1]/table/tbody/tr[2]/td[4]/span/a")).GetAttribute("title");
             String expectedResult = "Компоненти програмної інженерії 2. Якість та тестування програмного забезпечення";
-
             Assert.AreEqual(actualResult, expectedResult);
-            Thread.Sleep(2000);
+            Thread.Sleep(2000); 
         }
 
         [TearDown]
-        public void CloseBrowser()
+        public void TearDown()
         {
             driver.Close();
         }
